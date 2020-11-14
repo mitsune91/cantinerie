b7c69377139605a0b1a5148d214c81c203ca65c4
cedric <cedric.chok@gmail.com>
2020-11-14 14:33:28 +0100

    add example of how to call api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { HomeComponent } from './views/home/home.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomeComponent } from './views/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import {MenuService} from '../../services/menu.service';
+import {takeUntil} from 'rxjs/operators';
+import {BaseComponent} from '../../shared/core/base.component';
+import {Menu} from '../../models/Menu';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  menus: Array<Menu> = [];
+
+  constructor(private menuService: MenuService) {
+    super();
+  }
 
   ngOnInit(): void {
+    // Exemple d'appel api
+    // Ici je récupère tous les menus
+
+    // Appel au service MenuService
+    this.menuService.getMenus()
+      // Tant que la page n'est pas détruite,
+      .pipe(takeUntil(this.ngUnsubscribe))
+      // on souscrit à l'observable ou à la méthode getMenu()
+      .subscribe(data => {
+        // on stocke les données dans une variable pour les réutiliser
+        this.menus = data;
+        console.log(this.menus);
+    });
   }
 
 }
