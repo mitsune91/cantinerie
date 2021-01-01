9c8f7d03919960c6a266167884fa289f589813d3
cedric <cedric.chok@gmail.com>
2021-01-01 15:30:55 +0100

    edit-meal almost done: see TODO list

--- src/app/services/ingredient.service.ts
+++ src/app/services/ingredient.service.ts
@@ -19,6 +19,27 @@ import {Observable} from 'rxjs';
 })
 export class IngredientService {
 
+  categories = [
+    { code : 0, label: 'Inconnu' },
+    { code : 1, label: 'Apéritif' },
+    { code : 2, label: 'Entrée' },
+    { code : 3, label: 'Plat principal' },
+    { code : 4, label: 'Autre' },
+    { code : 5, label: 'Dessert' },
+    { code : 6, label: 'Brunch et déjeuner' },
+    { code : 7, label: 'Soupe' },
+    { code : 8, label: 'Sauce' },
+    { code : 9, label: 'Boisson' },
+    { code : 10, label: 'Sandwiche' },
+    { code : 11, label: 'Snack' },
+  ];
+
+  status = [
+    { code: 0, label: 'Actif' },
+    { code: 1, label: 'Inactif' },
+    { code: 2, label: 'Supprimé' }
+  ];
+
   constructor(private http: HttpClient) { }
 
   private url: string = HOST.apiUrl;
--- src/app/views/canteen/meal-manager/edit-meal/edit-meal.component.ts
+++ src/app/views/canteen/meal-manager/edit-meal/edit-meal.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {BaseComponent} from '../../../../shared/core/base.component';
+import {ActivatedRoute, Router} from '@angular/router';
 import {takeUntil} from 'rxjs/operators';
-import {MealService} from '../../../../services/meal.service';
 import {FormBuilder, FormGroup} from '@angular/forms';
+
+import {BaseComponent} from '../../../../shared/core/base.component';
+import {MealService} from '../../../../services/meal.service';
 import {HOST} from '../../../../../../config/app.config';
+import {IngredientService} from '../../../../services/ingredient.service';
 
 @Component({
   selector: 'app-edit-meal',
@@ -16,12 +18,19 @@ export class EditMealComponent extends BaseComponent implements OnInit {
   editedMeal: any = {};
   mealId: number;
   mealPathImg: string;
+  ingredients: any = [];
+  mealCategories = this.ingredientService.categories;
+  editedMealCategory: string;
+  editedMealStatus: string;
+  isFormDisplayed = false;
   form: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
     private mealService: MealService,
-    private fb: FormBuilder
+    private ingredientService: IngredientService,
+    private fb: FormBuilder,
+    private router: Router
     ) {
     super();
 
@@ -48,6 +57,9 @@ export class EditMealComponent extends BaseComponent implements OnInit {
         this.getMealById(this.mealId);
         this.getMealPathImgById(this.mealId);
       });
+    this.getAllIngredients();
+    // console.log(this.mealCategories);
+    console.log(this.editedMeal);
   }
 
   // Récupère un plat grâce à son id
@@ -57,29 +69,76 @@ export class EditMealComponent extends BaseComponent implements OnInit {
       .subscribe(meal => {
         this.editedMeal = meal;
         console.log(this.editedMeal);
+        this.getMealCategory(this.editedMeal);
+        this.getMealStatus(this.editedMeal);
         this.form.patchValue(this.editedMeal); // On insère les valeurs du plats dans le formulaire
       });
   }
 
+  // Récupère le path image d'un plat
   getMealPathImgById(id: number): void {
     this.mealService.getMealImg(id)
       .pipe(takeUntil(this.ngUnsubscribe))
       .subscribe(data => {
-        console.log(data);
         const apiUrl = HOST.apiUrl;
         this.mealPathImg = apiUrl + data.imagePath.split(' ').join('%20');
-        console.log(this.mealPathImg);
       });
   }
 
-  getValue(status: string): void {
-    console.log(this.form.value.status);
+  // Récupère la catégorie d'un plat
+  getMealCategory(meal: any): void {
+    this.mealCategories.forEach(cat => {
+      if (cat.code === meal.category) {
+        this.editedMealCategory = cat.label;
+        console.log(this.editedMealCategory);
+      }
+    });
   }
 
-  // The category for this element. unknown(0), appetizers(1), starters(2),
-  // main_dishes(3), others(4), desserts(5), brunchs_and_lunches(6),
-  // soups(7), sauces(8), drinks(9), sandwiches(10), snacks(11)
+  // Récupère la catégorie d'un plat
+  getMealStatus(meal: any): void {
+    this.ingredientService.status.forEach(status => {
+      if (status.code === meal.status) {
+        this.editedMealStatus = status.label;
+        console.log(this.editedMealStatus);
+      }
+    });
+  }
 
-  // The status for this element.
-  // 0 for Enabled, 1 for Disabled, 2 for Deleted
+  // Permet d'afficher le formulaire permettant de modifier un plat
+  displayForm(): void {
+    this.isFormDisplayed = !this.isFormDisplayed;
+  }
+
+  // Modifie le status via le formulaire
+  editMealStatus(status: any): void {
+    this.form.value.status = status;
+  }
+
+  // Envoie les changements du formulaire
+  submitEditedMeal(): void {
+    const body = this.form.value;
+    this.mealService.updateMealById(this.editedMeal.id, body)
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe();
+  }
+
+  // Récupère tous les ingrédients disponibles
+  getAllIngredients(): void {
+    this.ingredientService.getIngredients()
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(ingredients => {
+        this.ingredients = ingredients;
+      });
+  }
+
+  // Permet de revenir à la page de gestion des plats
+  onNavigateBack(): void {
+    this.router.navigate(['canteen/meals']);
+  }
+
+  // TODO Ajouter modal de confirmation pour modifier
+  // TODO Améliorer le Select des ingrédients
+  // TODO Travailler sur les disponibilités
+  // TODO Faire l'update d'image
 }
