4d3b1bd0831aab0bdfb8d13b857c05d112ebed92
cedric <cedric.chok@gmail.com>
2021-01-08 00:17:13 +0100

    css login page

--- src/app/views/auth/auth.component.ts
+++ src/app/views/auth/auth.component.ts
@@ -1,64 +1,70 @@
-import { Component, OnInit } from '@angular/core'
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'
-import { User } from 'src/app/models/User'
-import { AuthService } from 'src/app/services/auth.service'
-import { UserService } from 'src/app/services/user.service'
-import { isEmpty } from 'lodash-es'
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {User} from 'src/app/models/User';
+import {AuthService} from 'src/app/services/auth.service';
+import {UserService} from 'src/app/services/user.service';
+import {isEmpty} from 'lodash-es';
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
-  users: User[]
+  users: User[];
 
-  constructor (
+  constructor(
     private authService: AuthService,
     private formBuilder: FormBuilder,
     private usersService: UserService
-  ) {}
-  authForm: FormGroup
-  isSubmitted: boolean = false
+  ) {
+  }
 
-  ngOnInit (): void {
+  authForm: FormGroup;
+  isSubmitted = false;
+
+  ngOnInit(): void {
     this.authForm = this.formBuilder.group({
       email: ['', Validators.required],
       password: ['', Validators.required]
-    })
-    
-  }
-  get formControls () {
-    return this.authForm.controls
+    });
+
   }
 
-  getUsers (): User[] {
-    this.usersService.getUsers()
-    this.usersService.getUsers().subscribe(data => {
-      this.users = data
-    })
-    return this.users
+  get formControls(): any {
+    return this.authForm.controls;
   }
-  getUserByMail (mail: string) {
+
+  getUsers(): User[] {
+    this.usersService.getUsers();
+    this.usersService.getUsers().subscribe(data => {
+      this.users = data;
+    });
+    return this.users;
+  }
+
+  getUserByMail(mail: string): any {
     return this.getUsers()
       .filter(users => mail.indexOf(users.email) > -1)
       .filter(
         (elem1, pos, arr) =>
           arr.findIndex(elem2 => elem2.id === elem1.id) === pos
-      )
+      );
   }
-  onSubmit () {
-    const formValue = this.authForm.value
-    const mail = formValue['email']
-    const password = formValue['password']
+
+  onSubmit(): any {
+    const formValue = this.authForm.value;
+    const mail = formValue.email;
+    const password = formValue.password;
     if (this.authForm.valid) {
       if (!isEmpty(this.getUserByMail(mail))) {
-        let userLogged = this.getUserByMail(mail)
+        const userLogged = this.getUserByMail(mail);
         if (password === 'bonjour') {
-          this.authService.login(userLogged)
+          this.authService.login(userLogged);
         }
       }
     } else {
-      return this.authForm.controls
+      return this.authForm.controls;
     }
   }
 }
