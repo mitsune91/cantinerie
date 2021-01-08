d962815f1b23b44164cf1d77994336dbb61cdcdc
cedric <cedric.chok@gmail.com>
2021-01-08 15:14:17 +0100

    fix css

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,40 +1,61 @@
-import { Injectable } from '@angular/core'
-import { BehaviorSubject, Observable } from 'rxjs'
-import { User } from '../models/User'
-import { isEmpty } from 'lodash-es'
-import { Router } from '@angular/router'
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
 
-export const TOKEN_NAME: string = 'token_user'
+import { User } from '../models/User';
+
+export const TOKEN_NAME = 'token_user';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
- loggedIn = new BehaviorSubject<boolean>(false)
-  get isLoggedIn () {
-    return this.loggedIn.asObservable()
-  }
-  constructor (private router: Router) {}
+  loggedIn = new BehaviorSubject<boolean>(false);
 
-  getToken (): string {
-    return localStorage.getItem(TOKEN_NAME)
+  get isLoggedIn(): Observable<any> {
+    return this.loggedIn.asObservable();
   }
 
-  setToken (token: string): void {
-    localStorage.setItem(TOKEN_NAME, token)
+  constructor(
+    private router: Router,
+  ) {
   }
 
-  login (user: User[]) {
-    if (!isEmpty(user)) {
-      let userString = JSON.stringify(user)
-      this.setToken(userString)
-      this.loggedIn.next(true)
-      this.router.navigate(['/'])
+  /**
+   * return the current token
+   */
+  getToken(): string {
+    return localStorage.getItem(TOKEN_NAME);
+  }
+
+  /**
+   * Set the current token
+   * @param token
+   */
+  setToken(token: string): void {
+    localStorage.setItem(TOKEN_NAME, token);
+  }
+
+  /**
+   * Log the user
+   * @param user
+   */
+  login(user: User[]): void {
+    if (!!user) {
+      const userString = JSON.stringify(user);
+      this.setToken(userString);
+      this.loggedIn.next(true);
+      console.log('test')
+      // this.router.navigate(['/']);
     }
   }
 
-  logout () {
-    this.loggedIn.next(false)
-    localStorage.removeItem(TOKEN_NAME)
-    this.router.navigate(['/login'])
+  /**
+   * Logout the user
+   */
+  logout(): void {
+    this.loggedIn.next(false);
+    localStorage.removeItem(TOKEN_NAME);
+    this.router.navigate(['/login']);
   }
 }
